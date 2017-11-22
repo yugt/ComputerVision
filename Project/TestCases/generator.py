220de4d27e9a7db1fd0b90f2f61e3cbe47bf6cb1
Yu <yugtmath@umich.edu>
2017-11-22 13:45:26 -0500

    generator TODO integer answer selector

--- Project/TestCases/generator.py
+++ Project/TestCases/generator.py
@@ -1,6 +1,6 @@
 import os
 import random
-from math import log10, floor
+from math import log10, ceil
 random.seed(9001)
 
 
@@ -9,10 +9,12 @@ header+="\usepackage[margin=1in]{geometry}\n";
 header+="\usepackage{amsmath}\n";
 header+="\usepackage{multicol}\n";
 header+="\pagenumbering{gobble}\n";
+header+="\pagestyle{empty}";
 
 operatorslatex=("+","-","\\times","\div");
 operatorsplain=("+","-","*","/");
 
+# TODO: integer answer selector 
 def column_generator(row):
     c="\\begin{align*}\n";
     for i in range(0,row):
@@ -33,7 +35,7 @@ def page_generator(row,col):
 
 def files_generator(row, col, pages):
     for i in range(0,pages):
-        fname="tc"+str(i).zfill(int(floor(log10(pages)))+1)+".tex";
+        fname="tc"+str(i).zfill(int(ceil(log10(pages))))+".tex";
         f=open(fname,'w');
         f.write(page_generator(row,col));
         f.close()
@@ -47,12 +49,12 @@ def main(row,col,pages):
 
     m.write("\\begin{document}\n");
     for i in range(0,pages):
-        m.write("\include{tc"+str(i).zfill(int(floor(log10(pages)))+1)+"}\n")
+        m.write("\include{tc"+str(i).zfill(int(ceil(log10(pages))))+"}\n")
     m.write("\end{document}");
     m.close();
 
 log=open('check.txt','w');
-main(20,5,100);
+main(20,5,20);
 log.close();
 os.system("pdflatex main.tex");
-os.system("rm -f *.aux *.log ../.DS_Store tc*");
+os.system("rm -f *.aux *.log ../.DS_Store ../../.DS_Store tc*");
